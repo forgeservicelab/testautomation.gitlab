fc8932413ca47fae3b2ddf8bf98b100d4d6564f4
antti.koskinen <antti.koskinen@forgeservicelab.fi>
2014-08-14 14:17:48 +0300

    fix for creating new repo

--- GitClient.py
+++ GitClient.py
@@ -5,13 +5,14 @@ def removerepo(reponame):
     os.chdir('..')
     os.system('rm -rf ' + reponame)
 
-def initrepo(reponame, username, password):
+def initrepo(reponame, username, password, url):
     os.system('mkdir ' + reponame)
     os.chdir(reponame)
     os.system('git init')
     os.system('touch README')
     os.system('git add README')
     os.system('git commit -m "init"')
-    os.system('git remote add origin https://'+username+':'+password+'@gitlab-test.forgeservicelab.fi/'+username+'/' + reponame + '.git')
+    os.system('git remote add origin https://'+username+':'+password+'@' +url+'/'+username+'/' + reponame + '.git')
+    #os.system('git remote add origin https://'+username+':'+password+'@gitlab-test.forgeservicelab.fi/'+username+'/' + reponame + '.git')
     os.system('git push -u origin master')
 
